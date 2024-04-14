6d257b742b2debe58299ee7ffa9db9848373ec09
Myasnikov Mikhail <84364444+MyasnikovMikhail@users.noreply.github.com>
2024-04-15 00:11:54 +0700

    upd

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,19 +1,10 @@
 import hooks.WebHooks;
-import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.io.IOException;
-import java.net.URL;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pageElement.Asserts.DisappearingElementsPageAssert.isNotBreak;
 import static steps.CheckboxesSteps.checkAndClickCheckbox;
 import static steps.DisappearingElementsSteps.checkAndClickHomeButton;
@@ -21,7 +12,6 @@ import static steps.DropdownSteps.clickAndCheckDropdownList;
 import static steps.HoversSteps.hoverAndCheckAvatars;
 import static steps.InputsSteps.inputAndCheckText;
 import static steps.MainPageSteps.selectExample;
-import static utils.ConfProperties.getProperty;
 
 public class RunTest extends WebHooks {
     @Tag("all")
